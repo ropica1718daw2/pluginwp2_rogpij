0df3c71ff63f52efa87fbf709a4c3a1c60e831e5
Roger Pijoan Catala <ropica1718daw2@gmail.com>
2018-06-12 13:18:52 +0200

    Commit1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Roger Pijoan Catala
+Author URI: https://www.github.com/ropica1718daw2/
+License: GNU GPLv3 
+*/ 
+?>
