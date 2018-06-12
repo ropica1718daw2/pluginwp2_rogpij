d04e7c383ec865a5cdcfdffbbd8214063355d9c2
Roger Pijoan Catala <ropica1718daw2@gmail.com>
2018-06-12 21:51:32 +0200

    Commit2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,13 @@ Author: Roger Pijoan Catala
 Author URI: https://www.github.com/ropica1718daw2/
 License: GNU GPLv3 
 */ 
+
+function admin_menu_peu_pagina(){ 
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
