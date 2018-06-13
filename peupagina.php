91ebac9e97cc7d61b0476c73b0b3fe0f7ce42283
Roger Pijoan Catala <ropica1718daw2@gmail.com>
2018-06-13 09:35:34 +0200

    Commit 3

--- peupagina.php
+++ peupagina.php
@@ -14,7 +14,13 @@ function admin_menu_peu_pagina(){
 	$titolEntrada="Peu de pàgina per defecte";
 	$role="administrator";
 	$id="peu_pagina_per_defecte";
-	add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+	$nom_funcio="nouPeuPagDef";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);
 }
+
+function nouPeuPagDef(){
+	include_once('formPeuPag.php');
+}
+
 add_action('admin_menu','admin_menu_peu_pagina');
 ?>
