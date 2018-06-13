4ed7ad0e4afba48ac264fb8cce27fd72a2715c2f
Roger Pijoan Catala <ropica1718daw2@gmail.com>
2018-06-13 09:40:19 +0200

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -22,5 +22,12 @@ function nouPeuPagDef(){
 	include_once('formPeuPag.php');
 }
 
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+    font-size: 10px;
+    margin: 10px;'>".get_option('footer_text')."</div>";
+}
+
+add_action('wp_footer','afegPeuPagDef');
 add_action('admin_menu','admin_menu_peu_pagina');
 ?>
